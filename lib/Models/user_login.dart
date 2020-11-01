dbed7f6fdb674cf15d3f5011f22b0e1707f043dc
TsunMoon <kimhexi1998@gmail.com>
2020-11-01 17:43:09 +0700

    add new filter login

--- lib/Models/user_login.dart
+++ lib/Models/user_login.dart
@@ -2,8 +2,11 @@ class UserLogin {
   String username;
   String displayName;
   String photoUrl;
+  int amount;
+  int rating;
 
-  UserLogin({this.username, this.displayName, this.photoUrl});
+
+  UserLogin({this.username, this.displayName, this.photoUrl, this.amount, this.rating});
 
   
 
