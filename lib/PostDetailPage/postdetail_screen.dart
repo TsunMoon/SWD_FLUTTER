e568e3f70958ba6cceb83db6fc3d6024b3fc3385
TsunMoon <kimhexi1998@gmail.com>
2020-11-03 13:21:35 +0700

    thanh

--- lib/PostDetailPage/postdetail_screen.dart
+++ lib/PostDetailPage/postdetail_screen.dart
@@ -1,12 +1,18 @@
 import 'package:flutter/material.dart';
+import 'package:mobileapp/Models/user_login.dart';
 import 'package:mobileapp/Models/writer_post.dart';
 import 'package:mobileapp/PostDetailPage/components/body.dart';
 
 class PostDetailScreen extends StatelessWidget {
 
   WriterPost writerPost;
+  UserLogin userLogin;
+  int numberJob;
+  bool isRequested;
+  String strButton;
 
-  PostDetailScreen({Key key, this.writerPost}) : super(key: key);
+
+  PostDetailScreen({Key key, this.writerPost, this.userLogin, this.numberJob, this.isRequested,this.strButton }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
@@ -16,7 +22,7 @@ class PostDetailScreen extends StatelessWidget {
           'Post Detail'
         ),
       ),
-      body: Body(writerPost: writerPost,),
+      body: Body(writerPost: writerPost,userLogin: userLogin,numberJob: numberJob,isRequested: isRequested,strButton: strButton,),
     );
   }
 }
--- lib/global.dart
+++ lib/global.dart
@@ -1,7 +1,13 @@
+
+
 const GET_WRITER_POST = "https://thanhdeptrai.azurewebsites.net/api/posts/WriterPost";
 const GET_DESIGN_POST = "https://thanhdeptrai.azurewebsites.net/api/posts/DesignPost";
 const GET_TRANSLATE_POST = "https://thanhdeptrai.azurewebsites.net/api/posts/TranslatePost";
 
 const POST_LOGIN_USER = "https://thanhdeptrai.azurewebsites.net/api/users/loginUser";
+const POST_REQUESTED_POST = "https://thanhdeptrai.azurewebsites.net/api/UsersHavingPosts/requestedPost";
 
+const GET_REQUESTED_POST = "https://thanhdeptrai.azurewebsites.net/api/UsersHavingPosts/requestedPosts";
+const GET_ACCEPTED_POST = "https://thanhdeptrai.azurewebsites.net/api/UsersHavingPosts/acceptedPosts";
 
+const DELETE_REUESTED_POST = "https://thanhdeptrai.azurewebsites.net/api/UsersHavingPosts/removeRequestedPost";
